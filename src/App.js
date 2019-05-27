988bee768df0a8fccca094604e5a942e65898a66
Will Worth <will.worth@gmail.com>
2019-05-27 13:38:57 +0200

    scss and basic header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>reactconferencepicker</h1>
+      <Header/>
+      <h1>react conference picker</h1>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../styles/header.scss';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>  Frontend Developer Conferences </p>
+       
+      </div>
+    );
+  }
+}
+
+export default Header;
